751abfff82c19782fd46e17f2a3fe84f16a83c5f
mariosplen <mariosplenchidis@gmail.com>
2024-11-19 23:46:38 +0200

    improve inventory_page.dart ui

--- lib/features/inventory/presentation/inventory_page.dart
+++ lib/features/inventory/presentation/inventory_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 import 'package:multi_select_flutter/chip_display/multi_select_chip_display.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
@@ -111,105 +112,157 @@ class _InventoryPageState extends State<InventoryPage> {
               ),
             ],
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(inventory?.description ?? ''),
-              Text(inventory?.location ?? ''),
-              Text(inventory?.note ?? ''),
-              const Text('Filters'),
-              Row(
-                children: [
-                  MultiSelectDialogField(
-                    items: allCategories
-                            ?.map((category) =>
-                                MultiSelectItem(category, category))
-                            .toList() ??
-                        [],
-                    listType: MultiSelectListType.CHIP,
-                    chipDisplay: MultiSelectChipDisplay.none(),
-                    title: const Text('Categories'),
-                    buttonText: const Text('Category'),
-                    onConfirm: (values) {
-                      debugPrint("hi");
-                      setState(() {
-                        selectedCategories = [...values];
-                        1 + 1;
-                      });
-                    },
-                    // onSelectionChanged: (values) {
-                    //   debugPrint("hi");
-                    //   setState(() {
-                    //     selectedCategories = values as List<String>;
-                    //   });
-                    // },
-                    // onSaved: (values) {
-                    //   debugPrint("hi");
-                    //   setState(() {
-                    //     selectedCategories = [...?values];
-                    //     1 + 1;
-                    //   });
-                    // },
-                    initialValue: selectedCategories,
-                  ),
-                  MultiSelectDialogField(
-                    items: allTags
-                            ?.map((tag) => MultiSelectItem(tag, tag))
-                            .toList() ??
-                        [],
-                    listType: MultiSelectListType.CHIP,
-                    chipDisplay: MultiSelectChipDisplay.none(),
-                    title: const Text('Tags'),
-                    buttonText: const Text('Tags'),
-                    onConfirm: (values) {
-                      setState(() {
-                        selectedTags = [...values];
-                      });
-                    },
-                    initialValue: selectedTags,
-                  ),
-                  MultiSelectDialogField(
-                    items: allColors
-                            ?.map((color) => MultiSelectItem(color, color))
-                            .toList() ??
-                        [],
-                    listType: MultiSelectListType.CHIP,
-                    chipDisplay: MultiSelectChipDisplay.none(),
-                    title: const Text('Colors'),
-                    buttonText: const Text('Color'),
-                    onConfirm: (values) {
-                      setState(() {
-                        selectedColors = [...values];
-                      });
-                    },
-                    initialValue: selectedColors,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        statusIndex = (statusIndex + 1) % statuses.length;
-                      });
-                    },
-                    child: Text(statuses[statusIndex]),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: ListView(
-                  children: shownItems
-                      .map(
-                        (item) => ItemCard(
-                          item: item,
-                          isDetected: readerState.tagsFound
-                              .any((tag) => tag.epc == item.epc),
-                          onDeleteTap: () => _onDeleteItem(context, item),
-                          onEditTap: () => _onEditItem(context, item),
-                        ),
-                      )
-                      .toList(),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  inventory?.description ?? '',
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontWeight: FontWeight.bold,
+                      ),
                 ),
-              ),
-            ],
+                Text(
+                  inventory?.location ?? '',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                ),
+                Text(
+                  inventory?.note ?? '',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                ),
+                const Gap(24),
+                Text(
+                  'FILTERS',
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Theme.of(context).colorScheme.tertiaryContainer,
+                          ),
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            statusIndex = (statusIndex + 1) % statuses.length;
+                          });
+                        },
+                        child: Text('Display: ${statuses[statusIndex]}'),
+                      ),
+                      const Gap(4),
+                      MultiSelectDialogField(
+                        items: allCategories
+                                ?.map((category) =>
+                                    MultiSelectItem(category, category))
+                                .toList() ??
+                            [],
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .tertiaryContainer
+                              .withOpacity(0.5),
+                        ),
+                        listType: MultiSelectListType.CHIP,
+                        chipDisplay: MultiSelectChipDisplay.none(),
+                        title: const Text('Categories'),
+                        buttonText: const Text('Category'),
+                        onConfirm: (values) {
+                          debugPrint("hi");
+                          setState(() {
+                            selectedCategories = [...values];
+                          });
+                        },
+                        initialValue: selectedCategories,
+                      ),
+                      const Gap(4),
+                      MultiSelectDialogField(
+                        items: allTags
+                                ?.map((tag) => MultiSelectItem(tag, tag))
+                                .toList() ??
+                            [],
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .tertiaryContainer
+                              .withOpacity(0.5),
+                        ),
+                        listType: MultiSelectListType.CHIP,
+                        chipDisplay: MultiSelectChipDisplay.none(),
+                        title: const Text('Tags'),
+                        buttonText: const Text('Tags'),
+                        onConfirm: (values) {
+                          setState(() {
+                            selectedTags = [...values];
+                          });
+                        },
+                        initialValue: selectedTags,
+                      ),
+                      const Gap(4),
+                      MultiSelectDialogField(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .tertiaryContainer
+                              .withOpacity(0.5),
+                        ),
+                        items: allColors
+                                ?.map((color) => MultiSelectItem(color, color))
+                                .toList() ??
+                            [],
+                        listType: MultiSelectListType.CHIP,
+                        chipDisplay: MultiSelectChipDisplay.none(),
+                        title: const Text('Colors'),
+                        buttonText: const Text('Color'),
+                        onConfirm: (values) {
+                          setState(() {
+                            selectedColors = [...values];
+                          });
+                        },
+                        initialValue: selectedColors,
+                      ),
+                    ],
+                  ),
+                ),
+                const Gap(18),
+                Expanded(
+                  child: ListView(
+                    children: shownItems
+                        .map(
+                          (item) => ItemCard(
+                            item: item,
+                            isDetected: readerState.tagsFound
+                                .any((tag) => tag.epc == item.epc),
+                            onDeleteTap: () => _onDeleteItem(context, item),
+                            onEditTap: () => _onEditItem(context, item),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
--- lib/features/inventory/presentation/widgets/item_card.dart
+++ lib/features/inventory/presentation/widgets/item_card.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:rfid_inventory_app/core/models/item_model.dart';
 
 class ItemCard extends StatelessWidget {
@@ -16,37 +19,107 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Card(
-        color: isDetected ? Colors.green : Colors.white,
+    return Card(
+      color: Theme.of(context).colorScheme.onSecondary,
+      child: Container(
+        height: 180,
+        padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              children: [
-                Text(item.name),
-                Row(
-                  children: item.tags.map((tag) => Text(tag)).toList(),
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
                 ),
-              ],
-            ),
-            const Spacer(),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-            ),
-            Column(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: onEditTap,
+                child: Image.file(
+                  File(item.image),
+                  errorBuilder: (_, __, ___) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.tertiaryContainer,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.no_photography_rounded,
+                          size: 48,
+                          color: Theme.of(context).colorScheme.onError,
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: onDeleteTap,
-                ),
-              ],
+              ),
             ),
+            const Gap(8),
+            Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Text(
+                          item.name,
+                          overflow: TextOverflow.clip,
+                          maxLines: 1,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Gap(8),
+                  Text(
+                    'Category: ${item.category}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                  const Gap(4),
+                  Expanded(
+                    child: Wrap(
+                      children: List.generate(
+                        item.tags.length,
+                        (index) => Chip(
+                          label: Text(item.tags[index]),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.zero,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Spacer(),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.edit,
+                        ),
+                        onPressed: onEditTap,
+                        iconSize: 20,
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: onDeleteTap,
+                        iconSize: 20,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
